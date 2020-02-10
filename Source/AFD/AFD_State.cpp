a38629c343ce594d7662286fd0cf1dc6d2c807f1
fpalaciosFM <fpalacios.fm@gmail.com>
2020-02-10 05:55:57 -0600

    Tercer Limpieza de AFD

--- Source/AFD/AFD_State.cpp
+++ Source/AFD/AFD_State.cpp
@@ -4,6 +4,6 @@ AFD_State::AFD_State(int id) {
     this->id = id;
 }
 
-AFD_State::AFD_State(int id, unordered_set<AFTE_State*> conjunto) : AFD_State(id, final) {
+AFD_State::AFD_State(int id, unordered_set<AFTE_State*> conjunto) : AFD_State(id) {
     this->AFTE_Equivalent = conjunto;
 }
